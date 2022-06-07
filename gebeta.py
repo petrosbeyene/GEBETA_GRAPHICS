61b8b62b3db213d84f91d3de9877cf1691af29b5
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-06-07 10:04:25 +0300

    Update game logic & add functions
    Seeding loop, terminating points

--- gebeta.py
+++ gebeta.py
@@ -6,20 +6,26 @@ Traditional Ethiopian Game.
 # Import necessary modules
 import random
 
-def main():
-    """Main entry of the program."""
+current_player = round(random.random())
+bank1 = 0
+bank2 = 0
+player1 = [bank1,]
+player2 = [bank2,]
+players = [player1, player2]
+changed_idx = list()
+playing_board = list()
+upside_idx = list()
+downside_idx = list()
 
-    current_player = round(random.random())
-    bank1 = 0
-    bank2 = 0
-    player1 = [bank1,]
-    player2 = [bank2,]
-    players = [player1, player2]
-    changed_idx = list()
-    playing_board = list()
-    upside_idx = list()
-    downside_idx = list()    
-    
+def displayGame():
+    """A function to display the game"""
+    print(playing_board[0:6])
+    print(playing_board[6:12])
+    print("Current Player: Player{}".format(current_player + 1), end = "\t")
+    print("\tBank1:",players[0][0],"\tBank2:",players[1][0])
+
+def initBoard():
+    """A function to initialize the playing board."""
     for idx in range(12):
         playing_board.insert(idx, 4)    
 
@@ -27,40 +33,69 @@ def main():
         upside_idx.insert(idx2, idx2)
         downside_idx.insert(idx2 + 6, idx2 + 6)
 
-    while True:
-        # Show The Game
-        print(playing_board[0:6])
-        print(playing_board[6:12])
-        print("Bank1:",players[0] [0],"\tBank2:",players[1] [0])
-        print("Current Player:","Player"+str(current_player+1))
+def endGame():
+    """A function to mark the end of the game."""
+    pass
 
-        inp = int(input("Choose a pit from 1 to 12: "))
-        
-        if inp in upside_idx:
-            players [current_player].insert(1, upside_idx)
-            players [1 - current_player].insert(1, downside_idx)
-        elif inp in downside_idx:
-            players [current_player].insert(1, downside_idx)
-            players [1 - current_player].insert(1, upside_idx)
+def main():
+    """Main entry of the program."""
+    global current_player
+    initBoard()
+    print("Starting Player:","Player"+str(current_player + 1))
+    displayGame()
     
-        start_seed = inp
-        end_seed = start_seed + playing_board[inp - 1]
-        playing_board[inp - 1] = 0
-        changed_idx.append(inp)
-        # print(start_seed, end_seed) # debugger line
-
-        for seed in range(start_seed, end_seed):
-            if seed > 11:
-                seed = seed % 12
-            changed_idx.append(seed)
-            playing_board[seed] = playing_board[seed] + 1
-        
-        if playing_board[end_seed] == 1:
-            current_player = 1 - current_player
+    inp = int(input("Choose a pit from 1 to 12: "))
+    
+    if inp in upside_idx:
+        players [current_player].insert(1, upside_idx)
+        players [1 - current_player].insert(1, downside_idx)
+    elif inp in downside_idx:
+        players [current_player].insert(1, downside_idx)
+        players [1 - current_player].insert(1, upside_idx)
+    
+    # Game loop
+    while True:
+        if inp not in players[current_player][1]:
+            inp = int(input("Player{} - choose a pit again: ".format(current_player + 1)))
             continue
-        if playing_board[end_seed] == 4:
-            pass
 
+        start_seed = inp - 1
+
+        # Seeding loop 
+        while True:
+            end_seed = start_seed + playing_board[start_seed]
+            playing_board[start_seed] = 0
+            changed_idx.append(start_seed)
+            
+            # print(start_seed, end_seed) # debugger line
+            
+            for seed_idx in range(start_seed + 1, end_seed + 1):
+                if seed_idx > 11:
+                    seed_idx = seed_idx % 12
+                changed_idx.append(seed_idx)
+                playing_board[seed_idx] = playing_board[seed_idx] + 1
+                
+            # Debugger lines - to see each step
+            print(playing_board[0:6])
+            print(playing_board[6:12])
+            print()
+
+            if playing_board[seed_idx] == 1:
+                current_player = 1 - current_player
+                break
+            elif playing_board[seed_idx] == 4:
+                if seed_idx in players[current_player][1]:
+                    players[current_player][0] += 4
+                    current_player = 1 - current_player
+                elif seed_idx in players[1 - current_player][1]:
+                    players[1 - current_player][0] += 4
+                    current_player = 1 - current_player
+                break
+            elif playing_board[seed_idx] != 4 and playing_board[start_seed] != 1:
+                start_seed = seed_idx
+                continue
+        
+        # Check the end of the game
         sideZero = 0
         asideZero = 0
         for idx in players[current_player][1]:
@@ -72,13 +107,15 @@ def main():
         if asideZero == 6 and sideZero < 6:
             for seed in playing_board:
                 players[current_player][0] += seed
+                endGame()
                 break
         elif sideZero == 6 and asideZero < 6:
             for seed in playing_board:
                 players[1 - current_player] [0] += seed
+                endGame()
                 break
 
-        # ALGORITHM #7
+        # ALGORITHM #9
         for idx in range(12):
             if idx in changed_idx:
                 changed_seed = playing_board[idx]
@@ -86,15 +123,14 @@ def main():
                     if idx in players[current_player][1]:
                         players[current_player][0] += 4
                         playing_board[idx] = 0
-                        current_player = 1 - current_player
                         continue
                     elif idx in players[1 - current_player][1]:
                         players[1 - current_player][0] += 4
                         playing_board[idx] = 0
-                        current_player = 1 - current_player
                         continue
-        
-        # give the turn to the other player
-        current_player = 1 - current_player
-        
+
+        displayGame()
+        inp = int(input("Player{} - choose a pit from {} to {}: "
+            .format(current_player + 1, players[current_player][1][0] + 1, players[current_player][1][-1] + 1)))
+
 main()
