6c01adb708b373502b7b5e2fe76a5cc29282f225
petros beyene <petrosbeyene611@gmail.com>
2022-06-19 05:37:44 +0300

    Update Obj Viewer

--- OBJFileLoader/OBJFileLoader/objviewer.py
+++ OBJFileLoader/OBJFileLoader/objviewer.py
@@ -1,21 +1,21 @@
-import sys, pygame
+import sys
+import pygame
 from pygame.locals import *
 from pygame.constants import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
-# IMPORT OBJECT LOADER
+import time
+import tkinter
 from objloader import *
 import logic
 game = logic.Game()
 
+angle = 0
 def main():
     pygame.init()
     viewport = (900,600)
-    hx = viewport[0]/2
-    hy = viewport[1]/2
     srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
-    glClearColor(1.0, 1.0, 1.0, 1.0)
+    glClearColor(0.1, 0.1, 0.1, 0.0)
     glViewport(0, 0, 900, 600)
 
     glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
@@ -51,12 +51,25 @@ def main():
     data = logic.Data()
     beadPositionDict = data.getBeads()
 
+    brown = (210, 105, 30, 0)
+    blue = (0, 0, 128, 0)
+
+    # initialState()
     while True:
         if game.isEndGame == True:
-            endGame()
-            # time wait
+            end_text = endGame()
+            font = pygame.font.SysFont('freesansbold.ttf', 40)
+            if end_text == "Draw":
+                result_text = font.render("Draw", True, brown, blue)
+            else:
+                result_text = font.render("Winner : " + end_text, True, brown, blue)
+            text_data = pygame.image.tostring(result_text, "RGBA", True)
+            glWindowPos2d(300, 300)
+            glDrawPixels(result_text.get_width(), result_text.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, text_data)
+            pygame.display.flip()
+            time.sleep(5)
+            popWindow()
             break
-        # srf.fill(white)
         clock.tick(30)
         for e in pygame.event.get():
             if e.type == QUIT:
@@ -77,13 +90,12 @@ def main():
                 if e.button == 1: rotate = False
                 elif e.button == 3: move = False
 
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
 
         glTranslate(0, 0, - zpos)
         glRotate(90, 0, 0, 1)
-        glRotate(0, 0, 1, 0)
+        glRotate(angle, 0, 1, 0)
         board.render()
         glTranslate(0, 0, zpos)
 
@@ -91,9 +103,9 @@ def main():
             glTranslate(location[0], location[1], - zpos + location[2])
             bead.render()
             glTranslate(-location[0], -location[1], zpos - location[2])
+
         pygame.display.flip()
 
-    # code goes here
 
 
 def endGame():
@@ -103,4 +115,30 @@ def endGame():
         return game.player2.name
     elif game.player1.getBank() == game.player2.getBank():
         return 'Draw'
+
+def popWindow():
+    popWin = tkinter.Tk()
+    popWin.geometry("300x200")
+
+    play_button = tkinter.Button(popWin, text = "Play again", command=mainCaller())
+    play_button.pack(side = tkinter.RIGHT)
+    quit_button = tkinter.Button(popWin, text = "Quit", command = sys.quit())
+    quit_button.pack(side = tkinter.LEFT)
+    popWin.mainloop()
+
+def mainCaller():
+    main()
+
+# def initialState():
+#     start = tkinter.Tk()
+#     start.geometry("100X100")
+#     start_button = tkinter.Button(start, text = "Start", command = callMain())
+#     start_button.pack()
+#
+# def callMain():
+#     global angle
+#     angle = 0
+#
 main()
+# initialState()
+
